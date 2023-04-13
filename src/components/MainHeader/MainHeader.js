bbb09cc48aa05b0d7d642fd782a5a79384abaf7c
jo-duchan <jo_duchan@icloud.com>
2023-04-14 00:35:04 +0900

    section10-120

---
+++ src/components/MainHeader/MainHeader.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Navigation from "components/MainHeader/Navigation";
+import classes from "components/MainHeader/MainHeader.module.css";
+
+const MainHeader = (props) => {
+  return (
+    <header className={classes["main-header"]}>
+      <h1>A Typical Page</h1>
+      <Navigation
+        isLoggedIn={props.isAuthenticated}
+        onLogout={props.onLogout}
+      />
+    </header>
+  );
+};
+
+export default MainHeader;
