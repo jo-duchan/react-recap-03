cde3d9f8a255f587dea7c71db815e8adc7780e0b
jo-duchan <jo_duchan@icloud.com>
2023-04-23 00:58:04 +0900

    section10-130

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Card from "components/UI/Card/Card";
+import Button from "components/UI/Button/Button";
 import classes from "components/Home/Home.module.css";
 
 const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={props.onLogout}>Logout</Button>
     </Card>
   );
 };
