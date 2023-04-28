77d4cce2b26a53488d75f89266454748557d99ee
jo-duchan <jo_duchan@icloud.com>
2023-04-28 23:52:14 +0900

    section10-134

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Card from "components/UI/Card/Card";
 import Button from "components/UI/Button/Button";
 import classes from "components/Home/Home.module.css";
+import AuthContext from "store/auth-context";
 
 const Home = (props) => {
+  const authCtx = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>Logout</Button>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
   );
 };
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem("isLoggeIn");
+
+    if (userInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.setItem("isLoggeIn", "1");
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.removeItem("isLoggeIn");
+    setIsLoggedIn(true);
+  };
+  return (
+    <AuthContext.AuthContextProvider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.AuthContextProvider>
+  );
+};
 
 export default AuthContext;
